37351f6a213fea5e4351e795e4ef58265796c70a
Waleedd Elnozahy <walidelnozahy@gmail.com>
2019-12-16 16:57:49 +0300

    about page

---
+++ src/components/AboutContent/AboutContent.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import styled from "styled-components";
+import ReactPlayer from "react-player";
+import { Container } from "../_common/Container/Container";
+import { withTranslation } from "react-i18next";
+const AboutContent = ({
+  t,
+  firstHeading,
+  firstText,
+  secondHeading,
+  secondText,
+  thirdHeading,
+  thirdText,
+  featuredimage,
+  videoLink
+}) => {
+  const AboutContentWrapper = styled.div`
+    margin-top: 30px;
+  `;
+  const FirstSection = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    @media (max-width: 992px) {
+      grid-template-columns: 1fr;
+    }
+  `;
+  const TextDiv = styled.div``;
+  const ImageDiv = styled.div`
+    width: 100%;
+    height: 100%;
+    background-position: center;
+    background-size: cover;
+    background-image: url(${props => props.image});
+  `;
+  const SecondSection = styled.div`
+    margin-top: 40px;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 60px;
+    padding: 0 40px;
+    @media (max-width: 992px) {
+      grid-template-columns: 1fr;
+    }
+  `;
+  const SecondSectionText = styled.div``;
+  const VideoWrapper = styled.div`
+    margin-top: 40px;
+  `;
+
+  return (
+    <AboutContentWrapper>
+      <Container>
+        <FirstSection data-aos="fade-up">
+          <TextDiv>
+            <h2>{firstHeading}</h2>
+            <p>{firstText}</p>
+          </TextDiv>
+          {featuredimage ? (
+            <ImageDiv image={featuredimage.childImageSharp.fluid.src} />
+          ) : null}
+        </FirstSection>
+        <SecondSection>
+          <SecondSectionText data-aos="fade-right">
+            <h2>{secondHeading}</h2>
+            <p>{secondText}</p>
+          </SecondSectionText>
+          <SecondSectionText data-aos="fade-left">
+            <h2>{thirdHeading}</h2>
+            <p>{thirdText}</p>
+          </SecondSectionText>
+        </SecondSection>
+        <VideoWrapper data-aos="fade-up">
+          <ReactPlayer
+            url={videoLink}
+            controls
+            width={"100%"}
+            height={"600px"}
+          />
+        </VideoWrapper>
+      </Container>
+    </AboutContentWrapper>
+  );
+};
+export default withTranslation()(AboutContent);
