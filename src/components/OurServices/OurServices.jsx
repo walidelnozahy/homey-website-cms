2a6f109e09be8d1177c367597d801754b5638f6f
Waleedd Elnozahy <walidelnozahy@gmail.com>
2019-12-16 19:33:09 +0300

    added services

---
+++ src/components/OurServices/OurServices.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+import styled from "styled-components";
+import { Container } from "../_common/Container/Container";
+import { withTranslation } from "react-i18next";
+// import WrappedRegistrationForm from "../ContactForm/ContactForm";
+// import TitleYellow from "../TitleYellow/TitleYellow";
+import company from "../../_company/company";
+
+const OurServices = ({ t }) => {
+  const services = [
+    {
+      name: t("real estate consultant"),
+      text: t("real estate consultant description"),
+      icon:
+        "https://res.cloudinary.com/dqbgnn5hf/image/upload/v1571925884/real-estate.png"
+    },
+    {
+      name: t("property managment"),
+      text: t("property managment description"),
+      icon:
+        "https://res.cloudinary.com/dqbgnn5hf/image/upload/v1571925885/managment.png"
+    },
+    {
+      name: t("legal process"),
+      text: t("legal process description"),
+      icon:
+        "https://res.cloudinary.com/dqbgnn5hf/image/upload/v1571925884/legal.png"
+    },
+    {
+      name: t("university applications"),
+      text: t("university applications description"),
+      icon:
+        "https://res.cloudinary.com/dqbgnn5hf/image/upload/v1571925885/universty.png"
+    }
+  ];
+  const iconSize = "130px";
+  const OurServicesWrapper = styled.div`
+  margin-top 40px;
+  `;
+  const OurServicesInner = styled.div`
+    gap: 10px;
+
+    @media (max-width: 992px) {
+      grid-template-columns: 1fr;
+    }
+  `;
+  const ServicesWrapper = styled.div``;
+  const ServicesEachDiv = styled.div`
+    display: grid;
+    grid-template-columns: 20% 80%;
+    gap: 20px;
+    margin-top: 50px;
+    @media (max-width: 992px) {
+      grid-template-columns: 1fr;
+    }
+  `;
+  const IconWrapper = styled.div`
+    position: relative;
+    text-align: center;
+    display: flex;
+    background: transparent linear-gradient(180deg, #0f6193 0%, #124970 100%) 0%
+      0% no-repeat;
+    border-radius: 50%;
+    width: 150px;
+    height: 150px;
+    @media (max-width: 992px) {
+      margin: auto;
+    }
+  `;
+  //   const IconInner = styled.div`
+  //     max-width: 240px;
+  //     background: transparent linear-gradient(180deg, #0f6193 0%, #124970 100%) 0%
+  //       0% no-repeat;
+  //     border-radius: 0 0 100% 100%;
+  //   `;
+  const Image = styled.img`
+    width: 100px;
+    position: relative;
+    max-width: ${iconSize};
+    z-index: 2;
+    margin: auto;
+  `;
+  const TextWrapper = styled.div``;
+  const Title = styled.h1`
+    color: ${company.colorPrimary};
+  `;
+  const Text = styled.div``;
+  const RightColumn = styled.div``;
+  return (
+    <OurServicesWrapper>
+      <Container>
+        <OurServicesInner>
+          <ServicesWrapper>
+            {services.map(({ name, text, icon }, key) => {
+              return (
+                <ServicesEachDiv key={key}>
+                  <IconWrapper data-aos="fade-left">
+                    {/* <IconInner> */}
+                    <Image src={icon} />
+                    {/* </IconInner> */}
+                  </IconWrapper>
+                  <TextWrapper data-aos="fade-right">
+                    <Title>{name}</Title>
+                    <Text>{text}</Text>
+                  </TextWrapper>
+                </ServicesEachDiv>
+              );
+            })}
+          </ServicesWrapper>
+          {/* <RightColumn>
+            <TitleYellow title={t("let us contact you")} />
+            <WrappedRegistrationForm />
+          </RightColumn> */}
+        </OurServicesInner>
+      </Container>
+    </OurServicesWrapper>
+  );
+};
+
+export default withTranslation()(OurServices);
---
+++ src/templates/services-page.js
@@ -0,0 +1,100 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import Content, { HTMLContent } from '../components/Content'
+import HeaderPages from '../components/_common/HeaderPages/HeaderPages'
+import OurServices from '../components/OurServices/OurServices'
+
+export const ServicesPageTemplate = ({ title, content, contentComponent,
+  headerImage,
+  seo_title,
+  seo_desc,
+  firstHeading,
+  firstText,
+  secondHeading,
+  secondText,
+  thirdHeading,
+  thirdText,
+  featuredimage,
+  videoLink
+}) => {
+  const PageContent = contentComponent || Content
+
+  return (
+    <div>
+      <HeaderPages
+          title={title}
+          image={headerImage}
+        />
+        
+        <OurServices 
+       
+        />
+    {/* <section className="section section--gradient">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="section">
+              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                {title}
+              </h2>
+              <PageContent className="content" content={content} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section> */}
+    </div>
+  )
+}
+
+ServicesPageTemplate.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string,
+  contentComponent: PropTypes.func,
+}
+
+const ServicesPage = ({ data }) => {
+  const { markdownRemark: post } = data
+  console.log('ServicesPage',post)
+  return (
+    <Layout>
+      <ServicesPageTemplate
+        contentComponent={HTMLContent}
+        title={post.frontmatter.title}
+        headerImage={post.frontmatter.headerImage}
+        seo_title={post.frontmatter.seo_title}
+        seo_desc={post.frontmatter.seo_desc}
+       
+      />
+    </Layout>
+  )
+}
+
+ServicesPage.propTypes = {
+  data: PropTypes.object.isRequired,
+}
+
+export default ServicesPage
+
+export const ServicesPageQuery = graphql`
+  query ServicesPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+        seo_title
+        seo_desc
+        headerImage {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+       
+      }
+    }
+  }
+`
